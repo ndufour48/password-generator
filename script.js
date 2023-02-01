6ceac2c3a1fea274c876c28cc23f03d6a49a2f56
Nicholas DuFour <nick.dufour48@gmail.com>
2023-02-01 02:12:01 -0500

    added if statement to meet min critera / created internal func for promts

--- script.js
+++ script.js
@@ -39,47 +39,49 @@ function generatePassword(){
 
   while (passwordLength < 8 || passwordLength > 128) {
   passwordLength = prompt("Your password must be between 8 and 128 characters. \nHow many characters do you want your password to be?");
+  showPrompts();
+
+  while (pwCriteria.pwLength < passwordLength){
+
+    if (lowerCase === false && upperCase === false && numbers === false && specialChar === false) {
+      alert("Uh-oh! To successfully generate a password, you must choose to include one or more of the following: \nlowercase letters \nuppercase letters \nnumbers \nspecial characters")
+      showPrompts();
+    }
+
+    else {
+
+      if (lowerCase === true && pwCriteria.pwLength < passwordLength) {
+        var lc = pwCriteria.pwLowerCase[Math.floor(Math.random() *26)]
+        result = result + lc;
+        pwCriteria.pwLength = pwCriteria.pwLength + 1;
+      }
+
+      if (specialChar === true && pwCriteria.pwLength < passwordLength) {
+        var sc = pwCriteria.pwCharacter[Math.floor(Math.random() *32)]
+        result = result + sc;
+        pwCriteria.pwLength = pwCriteria.pwLength + 1;
+      }
+
+      if (upperCase === true && pwCriteria.pwLength < passwordLength) {
+        var uc = pwCriteria.pwUpperCase[Math.floor(Math.random() *26)]
+        result = result + uc;
+        pwCriteria.pwLength = pwCriteria.pwLength + 1;
+      }
+
+      if (numbers === true && pwCriteria.pwLength < passwordLength) {
+        var num = pwCriteria.pwNumber[Math.floor(Math.random() *10)]
+        result = result + num;
+        pwCriteria.pwLength = pwCriteria.pwLength + 1;
+      }
+    }
+  }
 }
 
+function showPrompts(){
   lowerCase = confirm("Would you like to include lower case letters? \nOK = yes \nCancel = no");
   upperCase = confirm("Would you like to include upper case letters? \nOK = yes \nCancel = no");
   numbers = confirm("Would you like to include numbers? \nOK = yes \nCancel = no");
   specialChar = confirm("Would you like to include special characters? \nOK = yes \nCancel = no");
-
-  while (pwCriteria.pwLength < passwordLength){
-
-    if (lowerCase === true && pwCriteria.pwLength < passwordLength) {
-      var lc = pwCriteria.pwLowerCase[Math.floor(Math.random() *26)]
-      result = result + lc;
-      pwCriteria.pwLength = pwCriteria.pwLength + 1;
-    }
-
-    if (specialChar === true && pwCriteria.pwLength < passwordLength) {
-      var sc = pwCriteria.pwCharacter[Math.floor(Math.random() *32)]
-      result = result + sc;
-      pwCriteria.pwLength = pwCriteria.pwLength + 1;
-    }
-
-    if (upperCase === true && pwCriteria.pwLength < passwordLength) {
-      var uc = pwCriteria.pwUpperCase[Math.floor(Math.random() *26)]
-      result = result + uc;
-      pwCriteria.pwLength = pwCriteria.pwLength + 1;
-    }
-
-    if (numbers === true && pwCriteria.pwLength < passwordLength) {
-      var num = pwCriteria.pwNumber[Math.floor(Math.random() *10)]
-      result = result + num;
-      pwCriteria.pwLength = pwCriteria.pwLength + 1;
-    }
-
-  }
-
-console.log (lc);
-console.log (uc);
-console.log (num);
-console.log (sc);
-console.log (result);
-console.log (result.length);
-
+}
 return result;
 }
