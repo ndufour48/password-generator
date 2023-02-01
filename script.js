b568e31ef7376786b33e5c0bca95170298250495
Nicholas DuFour <nick.dufour48@gmail.com>
2023-01-31 21:17:53 -0500

    added some local variables / updated promts

--- script.js
+++ script.js
@@ -2,18 +2,15 @@
 var generateBtn = document.querySelector("#generate");
 var pwCriteria = {
 
-  pwLength: "0",
+  pwLength: 0,
 
-  pwLowerCase:["a","b","c","d","e","f","g","h","i","j","k","l",
-    "m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+  pwLowerCase:["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
 
-  pwUpperCase:["A","B","C","D","E","F","G","H","I","J","K","L",
-  "M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
+  pwUpperCase:["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
 
-  pwNumber:[0,1,2,3,4,5,6,7,8,9],
+  pwNumber:[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
 
-  pwCharacter:["!","\"","#","$","%","&","\'","(",")","*","+",",",
-  "-",".","/","\\",":",";","<",">","=","?","@","[","]","^","_","`","{","}","|","~"]
+  pwCharacter:["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", "\\", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"]
 }
 
 // Write password to the #password input
@@ -29,14 +26,24 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword(){
-  var passwordLength = "0";
-  var specialChar = [];
+  var result = "";
+  var passwordLength = 0;
+  var upperCase;
+  var lowerCase;
+  var numbers;
+  var specialChar;
 
-    passwordLength = prompt("How many characters do you want your password to be?");
-  
-    alert("Would you like to include lower case letters?");
-    alert("Would you like to include upper case letters?");
-    alert("Would you like to include numbers?");
-    alert("Would you like to include special characters?");
+  passwordLength = 0;
+  pwCriteria.pwLength = 0;
+  result = "";
 
-  }
+  while (passwordLength < 8 || passwordLength > 128) {
+  passwordLength = prompt("Your password must be between 8 and 128 characters. \nHow many characters do you want your password to be?");
+}
+
+  lowerCase = confirm("Would you like to include lower case letters? \nOK = yes \nCancel = no");
+  upperCase = confirm("Would you like to include upper case letters? \nOK = yes \nCancel = no");
+  numbers = confirm("Would you like to include numbers? \nOK = yes \nCancel = no");
+  specialChar = confirm("Would you like to include special characters? \nOK = yes \nCancel = no");
+
+}
